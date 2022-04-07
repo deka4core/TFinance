8784373517246ff8eebed06aaf0fa54c7168475b
bzorn <v.klimenko.2137@gmail.com>
2022-04-07 23:50:04 +0300

    added command /favourites, /follow [stoсk_name]

--- database.py
+++ database.py
@@ -17,5 +17,22 @@ class Database:
     def add_user(self, user: User):
         self.cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?);",
                          (user.id, user.first_name, user.last_name,
-                          user.username, user.favourites_stocks, user.points))
+                          user.username, None, user.points))
         self.con.commit()
+
+    def add_favourites_stocks(self, user: User, stock_name: str):
+        stocks = self.cur.execute(f"SELECT favourites_stocks FROM users WHERE id = {user.id}").fetchone()
+        if stocks and stocks[0]:
+            stock_name = f'{stocks[0]} {stock_name}'
+        self.cur.execute(f"UPDATE users SET favourites_stocks = '{stock_name}' WHERE id = {user.id}")
+        self.con.commit()
+
+    def check_favourites_stocks(self, user, stock_name: str):
+        stocks = self.cur.execute(f"SELECT favourites_stocks FROM users WHERE id = {user.id}").fetchone()
+        if stocks[0] and stock_name in stocks[0]:
+            return True
+        return False
+
+    def get_favourites_stocks(self, user: User):
+        stocks = self.cur.execute(f"SELECT favourites_stocks FROM users WHERE id = {user.id}").fetchone()
+        return stocks
--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from graphics.visualize import pdr
 from telegram.ext import Updater, CommandHandler
 
 from items import User
+
+# Импортируем токен из другого файла safety_key.py.
 from safety_key import TOKEN
+
 from graphics.visualize import do_stock_image
 
-
+# Запускаем логирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
     filename="./logs/tfinance_main.log")
 logger = logging.getLogger(__name__)
+
 
 
 # Обработчик команды /stock [stock_index]
@@ -36,12 +40,38 @@ def help(update, context):
     update.message.reply_text("Я пока не умею помогать...")
 
 
+def favourites(update, context):
+    user_data = update.effective_user
+    user = User(user_data.to_dict())
+    stocks = Database('data.db').get_favourites_stocks(user)
+    if stocks:
+        update.message.reply_text(', '.join(stocks))
+    else:
+        update.message.reply_text('У вас нет избранных акций')
+
+
+def follow(update, context):
+    user_data = update.effective_user
+    user = User(user_data.to_dict())
+    if context.args[0]:
+        if Database('data.db').check_favourites_stocks(user, context.args[0]):
+            update.message.reply_text('Акция уже в избранном')
+        else:
+            Database('data.db').add_favourites_stocks(user, context.args[0])
+            update.message.reply_text('Акция добавлена в избранное')
+
+
 def main():
+    # Создаём объект updater.
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
+    # Регистрируем обработчик команд.
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("favourites", favourites))
+    dispatcher.add_handler(CommandHandler("follow", follow))
     dispatcher.add_handler(CommandHandler("stock", get_stock_image))
+    # Обработка сообщений.
     updater.start_polling()
     updater.idle()
 
