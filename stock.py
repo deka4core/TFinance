d9e5c403b384d200c6ae7d9ad557ff52002831d4
brandonzorn <68778953+brandonzorn@users.noreply.github.com>
2022-04-14 17:55:53 +0300

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,31 +1,5 @@
-import json
-import os
 import datetime
 import pandas_datareader as pdr
-import requests
-
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
-
-
-def get_all_stocks():
-    url = 'https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=6000&exchange=NASDAQ'
-    stocks = requests.get(url, headers=HEADERS).json().get('data').get('table').get('rows')
-    stocks = [i.get('symbol') for i in stocks]
-    save_stocks('stocks.json', stocks)
-
-
-def save_stocks(file_name, stocks: list):
-    wd = os.getcwd()
-    with open(f'{wd}/{file_name}', "w") as f:
-        json.dump({'stocks': stocks}, f)
-
-
-def load_stocks(file_name) -> list:
-    wd = os.getcwd()
-    if os.path.exists(f'{wd}/{file_name}'):
-        with open(f'{wd}/{file_name}') as f:
-            return json.load(f)
-    return []
 
 
 def check_stock(stock_name):
