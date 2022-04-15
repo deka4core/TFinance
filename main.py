ec0eeaa3db0acf13a92d830452479edbf33e6865
deka4core <borodaenglish@gmail.com>
2022-04-15 20:56:23 +0300

    Fixed /stocks [args] command.
    Added comments to main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+# ----------------------------------------- Основной файл приложения ----------------------------------------- #
+# --------------- Импорт необходимых библиотек, функций, классов --------------- #
+# Встроенные библиотеки.
 import logging
+import os
+
+# Для работы со временем.
 import datetime
 import pytz
-import os
+
+# Работа с telegram-bot-api.
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from database import Database
-from graphics.visualize import pdr, check_stock_prices
-from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
-from items import User
-# Импортируем токен из другого файла safety_key.py.
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler
+
+# Работа с акциями (загрузка, вывод, проверка, игра, визуализация).
+from graphics.visualize import do_stock_image, pdr, \
+    check_stock_prices
+from stock import check_stock, load_stocks, get_all_stocks
+
+from database import Database  # ORM (БД с данными о пользователях).
+from items import User  # Класс пользователя.
+
+# Импортируем TOKEN из безопасного места
 from safety_key import TOKEN
-from graphics.visualize import do_stock_image
-from stock import check_stock
 
 # Запускаем логирование
 if not os.path.exists(f'{os.getcwd()}/logs'):
@@ -21,6 +32,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# Отправка всех акций из заданного диапазона (start, end).
 def send_stocks(update, start, end, templates):
     msg = ''
     for i in range(start, end):
@@ -28,14 +40,18 @@ def send_stocks(update, start, end, templates):
     update.message.reply_text(msg)
 
 
+# Получение списка необходимых акций по команде /stocks [args]. Обработчик команды /stocks.
 def get_list_stocks(update, context):
     try:
-        templates = load_stocks('stocks.json')
+        templates = load_stocks('stocks.json')  # Загружаем список всех акций
+
+        # Проверка на наличие аргументов
         if context.args[0] == 'all':
+            # Большим сообщением все сразу не отправится, поэтому разделяем на 3 поменьше.
             send_stocks(update, 0, 700, templates)
             send_stocks(update, 700, 1400, templates)
             send_stocks(update, 1400, 2100, templates)
-        elif context.args[0].isdigit():
+        elif context.args[0].isdigit():  # Если аргумент - число, отсылаем первые n акций.
             numb = int(context.args[0])
             if numb > 700:
                 while numb > 700:
@@ -43,17 +59,17 @@ def get_list_stocks(update, context):
                     send_stocks(update, 0, numb, templates)
                     numb = int(context.args[0]) - 700
             send_stocks(update, 0, numb, templates)
-        else:
+        else:  # Если аргумент - строка, выводим все акции на первую букву строки.
             message = ''
             for el in templates['stocks']:
-                if el[0] == context.args[0]:
+                if el[0].lower() == context.args[0].lower():
                     message += el + ', '
             update.message.reply_text(message)
     except (IndexError, ValueError):
         update.message.reply_text("Неверный способ ввода. /help")
 
 
-# Обработчик команды /stock [stock_index]
+# Обработчик команды /stock [stock_index]. Визуализирует график и отправляет его в прямом порядке.
 def get_stock_image(update, context):
     try:
         update.message.reply_photo(do_stock_image(context.args[0]))
@@ -63,22 +79,24 @@ def get_stock_image(update, context):
         update.message.reply_text("Такой акции не было найдено в данных Yahoo Finance.")
 
 
-def start(update, context):
-
+# Обработчик команды /start. Добавляет пользователя в БД, тем самым открывая ему доступ к командам.
+def start(update, _):
     user_data = update.effective_user
     user = User(user_data.to_dict())
     Database('data.db').add_user(user)
     update.message.reply_text(f"Привет, {user.first_name}! Теперь доступ к командам открыт.")
 
 
-def help(update, context):
+# Обработчик команды /help. ToDo: Сделать вывод всех команд и пояснение к ним.
+def help(update, _):
     update.message.reply_text("/stocks [количество акций] Например: /stocks 100 - посмотреть первые 100 акций\n"
                               "/stocks all - посмотреть все акции на бирже\n"
                               "/stocks [буква алфавита] Например: /stocks A - посмотреть все акции, "
                               "название которых начинается с 'A'")
 
 
-def favourites(update, context):
+# Обработчик команды /favourites. Отправляет список любимых акций.
+def favourites(update, _):
     user_data = update.effective_user
     user = User(user_data.to_dict())
     stocks = Database('data.db').get_favourites_stocks(user)
@@ -88,6 +106,7 @@ def favourites(update, context):
         update.message.reply_text('У вас нет избранных акций')
 
 
+# Обработчик команды /follow. Возможность подписываться на другие акции.
 def follow(update, context):
     user_data = update.effective_user
     user = User(user_data.to_dict())
@@ -101,8 +120,11 @@ def follow(update, context):
                 update.message.reply_text('Акция добавлена в избранное')
             else:
                 update.message.reply_text('Акция не найдена')
+    else:
+        update.message.reply_text('Неверный способ ввода. /follow [индекс акции]. Например: /follow AAPL')
 
 
+# Обработчик команды /unfollow. Возможность отписки от акций.
 def unfollow(update, context):
     user_data = update.effective_user
     user = User(user_data.to_dict())
@@ -113,11 +135,14 @@ def unfollow(update, context):
         else:
             Database('data.db').remove_favourites_stock(user.id, context.args[0])
             update.message.reply_text('Акция удалена из избранного')
+    else:
+        update.message.reply_text('Неверный способ ввода. /unfollow [индекс акции].')
 
 
+# Ежедневная рассылка избранных акций.
 def notify_assignees(context):
     db = Database('data.db')
-    for user in db.get_users():
+    for user in db.get_users():  # Перебираем всех пользователей и рассылаем каждому курсы их избранных акций.
         if db.check_user_daily_notify(user.id):
             if user.favourites_stocks:
                 for i in user.favourites_stocks.split():
@@ -127,7 +152,8 @@ def notify_assignees(context):
                         print(e)
 
 
-def daily(update, context):
+# Обработчик команды /daily. Включение/выключение ежедневной рассылки.
+def daily(update, _):
     user_data = update.effective_user
     user = User(user_data.to_dict())
     Database('data.db').add_user(user)
@@ -139,7 +165,8 @@ def daily(update, context):
     Database('data.db').user_daily_notify(user_id)
 
 
-def stats(update, context):
+# Обработчик команды /stats. Вывод статистики пользователя из БД.
+def stats(update, _):
     user_data = update.effective_user
     user = User(user_data.to_dict())
     data = Database('data.db').read_info(user)
@@ -151,7 +178,7 @@ def stats(update, context):
         update.message.reply_text('Вас нет в бд, запустите команду /start чтобы исправить ошибку')
 
 
-# Меню игры
+# Обработчик команды /game [stock_index]. Основное меню игры с предугадыванием.
 def game_menu(update, context):
     try:
         if not context.args:
@@ -161,22 +188,19 @@ def game_menu(update, context):
         user_data = update.effective_user
         user = User(user_data.to_dict())
         data = Database('data.db')
-        message_id = str(int(update.message.message_id) + 2)
+        message_id = str(int(update.message.message_id) + 2)  # Сохраняем id сообщения для возможности одновременной
+        # игры на многих акциях. Прибавляем 2 т.к. отправляем 2 сообщения: фото и приписку к нему с клавиатурой.
 
-        if data.get_selected_stock(user, message_id):
-            update.message.reply_text("Выполните прежний прогноз, чтобы начать новый! "
-                                      "Чтобы отменить его нажмите выход.")
-            return ConversationHandler.END
+        data.select_stock(user, f"{context.args[0]}:{message_id}")  # Запоминаем, что акция была выбрана.
 
-        data.select_stock(user, f"{context.args[0]}:{message_id}")
-
-        if Database('data.db').check_prediction_stock(user, context.args[0]):
+        if Database('data.db').check_prediction_stock(user, context.args[0]):  # Избегаем читерства.
             update.message.reply_text("Прогноз на эту акцию уже установлен.")
             return ConversationHandler.END
 
+        # Создание клавиатуры и отправка ответа.
         keyboard = [[
-                InlineKeyboardButton("Повышение", callback_data=str(1)),
-                InlineKeyboardButton("Понижение", callback_data=str(2))]]
+            InlineKeyboardButton("Повышение", callback_data=str(1)),
+            InlineKeyboardButton("Понижение", callback_data=str(2))]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_photo(photo=do_stock_image(context.args[0]))
         update.message.reply_text(text=f"Предугадайте курс {context.args[0]} на завтра.", reply_markup=reply_markup)
@@ -189,48 +213,49 @@ def game_menu(update, context):
         update.message.reply_text(text="Такой акции не было найдено в данных Yahoo Finance.")
         return ConversationHandler.END
 
-    return 1
+    return 1  # Возвращаем 1, чтобы показать ConversationHandler'у состояние, в котором находимся.
 
 
-# Кнопка повышения акции
-def higher_game(update, context):
+# Кнопка повышения акции.
+def higher_game(update, _):
     user_data = update.effective_user
     user = User(user_data.to_dict())
     data = Database('data.db')
 
+    # Обработка и ответ на колл-бэк.
     query = update.callback_query
     query.answer()
 
-    message_id = query.message.message_id
-    data.add_prediction(user, data.get_selected_stock(user, message_id), 'up')
-    data.remove_selected_stock(user, message_id)
+    message_id = query.message.message_id  # Получаем id сообщения, для нахождения нужной сессии игры.
+    data.add_prediction(user, data.get_selected_stock(user, message_id), 'up')  # Устанавливаем прогноз на акцию.
+    data.remove_selected_stock(user, message_id)  # Удаляем акцию из выбранных.
 
-    query.edit_message_text(text="Предсказание установлено на повышение")
+    query.edit_message_text(text="Предсказание установлено на повышение")  # Редактируем сообщение с клавиатурой.
 
 
-# Кнопка понижения акции
-def lower_game(update, context):
+# Кнопка понижения акции.
+def lower_game(update, _):
     user_data = update.effective_user
     user = User(user_data.to_dict())
     data = Database('data.db')
 
+    # Обработка и ответ на колл-бэк.
     query = update.callback_query
     query.answer()
 
-    message_id = query.message.message_id
-    data.add_prediction(user, data.get_selected_stock(user, message_id), 'down')
-    data.remove_selected_stock(user, message_id)
+    message_id = query.message.message_id  # Получаем id сообщения, для нахождения нужной сессии игры.
+    data.add_prediction(user, data.get_selected_stock(user, message_id), 'down')  # Устанавливаем прогноз на акцию.
+    data.remove_selected_stock(user, message_id)  # Удаляем акцию из выбранных.
 
-    query.edit_message_text(text="Предсказание установлено на понижение")
+    query.edit_message_text(text="Предсказание установлено на понижение")  # Редактируем сообщение с клавиатурой.
 
 
-# Подсчитывает результаты игры
+# Подсчет результатов игры.
 def game_results(context):
     db = Database('data.db')
     for user in db.get_users():
         for i in user.prediction.split():
-
-            # Проверяем прогноз
+            # Проверка прогноза
             if i.split(":")[-1] == 'up':
                 if check_stock_prices(i.split(":")[0]):
                     context.bot.send_message(chat_id=user.id, text=f"Прогноз {i.split(':')[0]} был верным. "
@@ -255,21 +280,32 @@ def game_results(context):
         user.prediction = db.get_predictions(user)
 
 
+# Основной цикл, активирующийся при запуске.
 def main():
+    # Получение и сохранение списка всех акций в stocks.json.
+    try:
+        get_all_stocks()
+    except Exception as e:
+        logging.error(e)
+
     # Создаём объект updater.
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
     job_queue = updater.job_queue
+
+    # Ежедневные задачи.
     t = datetime.time(hour=20, minute=19, second=40, tzinfo=pytz.timezone('Europe/Moscow'))
     job_queue.run_daily(notify_assignees, t)
     job_queue.run_daily(game_results,
                         datetime.time(hour=3, tzinfo=pytz.timezone('Europe/Moscow')))
 
-    conv_handler = ConversationHandler(entry_points=[CommandHandler("game", game_menu)],
+    # ConversationHandler для игры.
+    game_handler = ConversationHandler(entry_points=[CommandHandler("game", game_menu)],
                                        states={1: [CallbackQueryHandler(higher_game, pattern=f"^1$"),
                                                    CallbackQueryHandler(lower_game, pattern=f"^2$")]},
                                        fallbacks=[CommandHandler("game", game_menu)])
-    dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(game_handler)
+
     # Регистрируем обработчик команд.
     dispatcher.add_handler(CommandHandler("daily", daily))
     dispatcher.add_handler(CommandHandler("start", start))
@@ -280,6 +316,7 @@ def main():
     dispatcher.add_handler(CommandHandler("stock", get_stock_image))
     dispatcher.add_handler(CommandHandler("stocks", get_list_stocks))
     dispatcher.add_handler(CommandHandler("stats", stats))
+
     # Обработка сообщений.
     updater.start_polling()
     updater.idle()
--- stock.py
+++ stock.py
@@ -1,5 +1,19 @@
 import datetime
+import json
+import os
+
 import pandas_datareader as pdr
+import requests
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
+
+
+def load_stocks(file_name) -> list:
+    wd = os.getcwd()
+    if os.path.exists(f'{wd}/{file_name}'):
+        with open(f'{wd}/{file_name}') as f:
+            return json.load(f)
+    return []
 
 
 def check_stock(stock_name):
@@ -10,3 +24,18 @@ def check_stock(stock_name):
         return True
     except Exception as e:
         return False
+
+
+def save_stocks(file_name, stocks: list):
+    wd = os.getcwd()
+    with open(f'{wd}/{file_name}', "w") as f:
+        json.dump({'stocks': stocks}, f)
+
+
+def get_all_stocks():
+    url = 'https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=6000&exchange=NASDAQ'
+    stocks = requests.get(url, headers=HEADERS).json().get('data').get('table').get('rows')
+    stocks = [i.get('symbol') for i in stocks]
+    save_stocks('stocks.json', stocks)
+
+
