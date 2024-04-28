05a1cc05f0980a461f3c6124a2f6c4e1acd9458b
bzorn <v.klimenko.2137@gmail.com>
2024-04-28 10:57:45 +0300

    async python-telegram-bot 21

--- blast.py
+++ blast.py
@@ -1,12 +1,15 @@
 # Ежедневная рассылка избранных акций.
 import logging
 
+from telegram import Update
+from telegram.ext import CallbackContext
+
 from database import Database
 from functions import create_user
 from graphics.visualize import do_stock_image
 
 
-def notify_assignees(context):
+async def notify_assignees(context: CallbackContext):
     db = Database("data.db")
     # Перебираем всех пользователей и рассылаем каждому курсы их избранных акций.
     for user in db.get_users():
@@ -14,18 +17,18 @@ def notify_assignees(context):
             if user.favourites_stocks:
                 for i in user.favourites_stocks.split():
                     try:
-                        context.bot.send_photo(chat_id=user.id, photo=do_stock_image(i))
+                        await context.bot.send_photo(chat_id=user.id, photo=do_stock_image(i))
                     except Exception as e:
                         logging.exception(e)
 
 
 # Включение/выключение ежедневной рассылки.
-def daily(update, _):
+async def daily(update: Update, _):
     db = Database("data.db")
     user = create_user(update)
     db.add_user(user)
     if db.check_user_daily_notify(user):
-        update.message.reply_text("Ежедневная рассылка выключена")
+        await update.message.reply_text("Ежедневная рассылка выключена")
     else:
-        update.message.reply_text("Ежедневная рассылка включена")
+        await update.message.reply_text("Ежедневная рассылка включена")
     db.user_daily_notify(user)
