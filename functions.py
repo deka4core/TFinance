78bd09a2d271b44d0c43c036395c1ba8af116b87
bzorn <v.klimenko.2137@gmail.com>
2024-06-21 09:34:22 +0300

    fixed if last name is none

--- functions.py
+++ functions.py
@@ -6,6 +6,6 @@ from models import User
 def create_user(update: Update) -> User:
     user_data = update.effective_user.to_dict()
     return User(
-        user_data["id"], user_data["first_name"], user_data["last_name"],
+        user_data["id"], user_data.get("first_name"), user_data.get("last_name"),
         user_data["username"], user_data["language_code"], user_data["is_bot"],
     )
